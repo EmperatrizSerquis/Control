ce8ad0bb839786ea9e82643661f1a8f40e73e3ba
EmperatrizSerquis <59320647+EmperatrizSerquis@users.noreply.github.com>
2023-10-29 12:51:40 -0300

    adding components

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import CategoryItem from './CategoryItem/CategoryItem'
-import Header from './Header/Header'
-import PaymentItem from './PaymentItem/PaymentItem'
-import DatePick from './DatePick/DatePick'
-
-
-export { CategoryItem, Header, PaymentItem, DatePick }
