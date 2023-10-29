6d7b652d07d83459220c4d7c3bcb3cd268dcbae6
EmperatrizSerquis <59320647+EmperatrizSerquis@users.noreply.github.com>
2023-10-29 13:00:36 -0300

    new services and utils

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
-import paymentsSlice from '../features/payments/paymentsSlice'
-import todaySlice from '../features/todayDate/todaySlice'
-
+import LenguageSlice from '../features/LenguageSlice'
+import { authApi } from '../services/AuthService'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import AuthSlice from '../features/AuthSlice'
+import { infoUserApi } from '../services/InfoUserService'
+import { managementApi } from '../services/ManagementService'
 
 export const store = configureStore({
-  reducer: {
-    payments: paymentsSlice,
-    today: todaySlice,
-  },
+    reducer: {
+        lenguage: LenguageSlice,
+        auth: AuthSlice,
+        [authApi.reducerPath]: authApi.reducer,
+        [infoUserApi.reducerPath]: infoUserApi.reducer,
+        [managementApi.reducerPath]: managementApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(authApi.middleware, infoUserApi.middleware, managementApi.middleware),
 })
+
+setupListeners(store.dispatch)
