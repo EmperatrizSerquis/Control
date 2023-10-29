ee4f3dc612de9ea4b7513bd226462118c7b7a557
EmperatrizSerquis <59320647+EmperatrizSerquis@users.noreply.github.com>
2023-10-29 12:57:51 -0300

    adding screens

--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-import Categories from './Categories/Categories'
-import Home from './Home/Home'
-import Payments from './Payments/Payments'
-import CategoriesDetail from './CategoriesDetail/CategoriesDetail'
-import PaymentsDetail from './PaymentsDetail/PaymentsDetail'
-
-export { Home, Categories, Payments, CategoriesDetail, PaymentsDetail }
